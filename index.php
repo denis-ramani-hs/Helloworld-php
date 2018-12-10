c5e7f955dadd0170aae1780b28c76b766b0fd770
denis-ramani-hs <45007164+denis-ramani-hs@users.noreply.github.com>
2018-12-10 15:35:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
-echo "<body style='background-color:pink'>";
+echo "<body style='background-color:#1f78c1'>";
 echo "hello world!!! ";
 echo "<br/>";
 
 echo "Welcome to First website.";
+echo "<br/>";
 echo "From Jenkins";
