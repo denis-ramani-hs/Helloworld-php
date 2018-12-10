3d40d1f8e474668825f7f011757adb26004a73f9
denis-ramani-hs <45007164+denis-ramani-hs@users.noreply.github.com>
2018-12-10 16:00:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,9 @@ echo "<body style='background-color:#4169E1'>";
 echo "hello world!!! ";
 echo "<br/>";
 
-echo "Welcome to First website.";
+echo "<h1><center> Welcome to First website.";
 echo "<br/>";
 echo "From Jenkins";
-echo "ABCDE"
+echo "ABCDE";
+echo "<br/>";
+echo "xyz</center></h1>";
