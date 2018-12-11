cc31294fca3bbcfc9e7acc9506df1012b939dc86
denis-ramani-hs <45007164+denis-ramani-hs@users.noreply.github.com>
2018-12-11 17:44:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 echo "<body style='background-color:pink'>";
-echo "hello world!!! ";
-echo "<br/>";
 
 echo "<h1><center> Welcome to First website.";
-echo "<br/>";
-echo "From Jenkins";
-echo "ABCDE";
-echo "<br/>";
-echo "xyz</center></h1>";
+echo "hello world!!! <br/>";
+echo "This change is From Jenkins. <br/>";
+echo "Denis Ramani</center></h1>";
